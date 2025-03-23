ece70bb1f2e69f5e38b52375d24d88d23797c277
HGrewal13 <harrysgrewal13@gmail.com>
2025-03-23 14:55:31 -0400

    Included ERD of all tables. SQL file to create databases also included.

---
+++ Final_Files/table_creation.sql
@@ -0,0 +1,37 @@
+create table category (
+	category_id varchar(255) PRIMARY KEY,
+	category varchar(255)
+);
+
+create table subcategory (
+	subcategory_id varchar(255) PRIMARY KEY,
+	subcategory varchar(255)
+);
+
+
+create table campaign (
+	cf_id INT,
+	contact_id INT PRIMARY KEY,
+	company_name varchar(255),
+	description varchar(255),
+	goal Float,
+	pledged Float,
+	outcome varchar(255),
+	backers_count INT,
+	country varchar(255),
+	currency varchar(255),
+	launch_date Date,
+	end_date Date,
+	category_id varchar(255),
+	subcategory_id varchar(255),
+	foreign key (category_id) references category(category_id),
+	foreign key (subcategory_id) references subcategory(subcategory_id)
+);
+
+create table contacts (
+	contact_id INT,
+	first_name varchar(255),
+	last_name varchar(255),
+	email varchar(255),
+	foreign key (contact_id) references campaign(contact_id)
+);
